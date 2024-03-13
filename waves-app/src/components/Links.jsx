7aec59e56c77d2ae1996e4fb630fb843972264c7
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-13 12:44:12 -0400

    Update Links.jsx

--- waves-app/src/components/Links.jsx
+++ waves-app/src/components/Links.jsx
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const links = [
-    { url: '/search', text: 'All'},
-    { url: '/news', text: 'News'},
-    { url: '/images', text: 'Images'},
-    { url: '/videos', text: 'Videos'}
-]
+  { url: '/search', text: '🔍 All' },
+  { url: '/news', text: '📰 News' },
+  { url: '/images', text: 'Images' },
+  { url: '/videos', text: '📼 Videos' },
+];
 
-export const Links = () => {
-    return (
-        <div className='flex sm:justify-around justify-between items-center mt-4'>
-            {links.map(({ url, text }) => {
-                <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-400 border-blue-700 pb-2">
-                    {text}
-                </NavLink>
-            })}
-        </div>
-    )
-}
+export const Links = () => (
+  <div className="flex sm:justify-around justify-between items-center mt-4">
+    {links.map(({ url, text }) => (
+      <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2">{text}</NavLink>
+    ))}
+  </div>
+);
