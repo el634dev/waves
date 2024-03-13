a8cf2c5861f487f3e1afb0558974cad53237365d
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-13 12:34:41 -0400

    Added links

---
+++ waves-app/src/components/Links.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    { url: '/search', text: 'All'},
+    { url: '/news', text: 'News'},
+    { url: '/images', text: 'Images'},
+    { url: '/videos', text: 'Videos'}
+]
+
+export const Links = () => {
+    return (
+        <div className='flex sm:justify-around justify-between items-center mt-4'>
+            {links.map(({ url, text }) => {
+                <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-400 border-blue-700 pb-2">
+                    {text}
+                </NavLink>
+            })}
+        </div>
+    )
+}
