da67d0603593b7b00cbe04913a8d1a065465165a
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-15 14:28:33 -0400

    Update MenuIcon.jsx

--- src/components/MenuIcon.jsx
+++ src/components/MenuIcon.jsx
@@ -2,7 +2,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 const MenuIcon = () => {
     return (
-        <div className="flex gap-2 bg-gray-500 rounded">
+        <div className="flex gap-2 bg-gray-500 sm:ml-2 rounded">
             <span className="h-10 w-10 flex items-center justify-center rounded-full cursor-pointer hover:bg-black/[0.05]">
                 <RxHamburgerMenu size={20} color="#AAAAAA" />
             </span>
