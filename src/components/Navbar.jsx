f76a1d88b030a2073504e5c0fc911e12de637253
el634dev <84924260+el634dev@users.noreply.github.com>
2024-11-08 14:28:03 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import MenuIcon from './MenuIcon';
+import { CiBellOn } from "react-icons/ci";
+import SearchInput from './SearchInput';
 
-const Navbar = () => {
+export default function Navbar() {
     return (
-        <header className='h-16 flex justify-between md:justify-end items-center gap-4 px-4'>
+        <header className='h-16 flex justify-between md:justify-end items-center gap-4 px-4 '>
+           <div className='mr-[22rem] flex justify-between mt-8'>
+             {/* Company Name */}
+             <h1 className="border-gray-400 font-bold text-4xl mt-2 mr-6 text-blue-900">WAVES</h1>
+             <SearchInput />
+           </div>
             {/* Links next to hamburger menu */}
-            <div className='flex gap-4'>
-                <span className='text-black/[0.87] text-[15px] text-blue-300 line-height hover:underline cursor-pointer'>
-                    Privacy
-                </span>
+            <div className='flex gap-4 mt-8'>
+                <button className='bg-blue-800 text-[1rem] text-white p-[0.50rem] rounded line-height hover:underline cursor-pointer flex items-center'>
+                    <CiBellOn className='mr-1'/>  News
+                </button> 
                 {/* Gift */}
-                <span className='text-black/[0.87] text-[15px] text-blue-300 line-height hover:underline cursor-pointer'>
-                    Gift the ocean
-                </span>
+                <button className='bg-blue-800 text-[1rem] text-white p-[0.50rem] rounded line-height hover:underline cursor-pointer'>
+                    Gift the Ocean
+                </button>
             </div>
             {/* Hamburger Icon */}
             <MenuIcon />
         </header>
     );
 };
-
-export default Navbar;
