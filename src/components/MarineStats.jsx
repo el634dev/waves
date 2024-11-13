8735967ee98e5786796d9cbfd4a564719be4e553
el634dev <84924260+el634dev@users.noreply.github.com>
2024-11-12 21:26:18 -0500

    Updated MarineStats.jsx

--- src/components/MarineStats.jsx
+++ src/components/MarineStats.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import StatsItem from './StatsItem';
-import { GiWaterBottle } from "react-icons/gi";
+import { BiMoneyWithdraw } from "react-icons/bi";
 
 export default function Stats() {
     const statsData = [
         { 
-            value: "$80,000,000",
-            description: "dedicated to marine preservation & climate action"
+            value: "80,000,000,000",
+            description: "billion dollars donated by Waves Search"
         }
-    ]
+    ];
+    
     return (
-        <section className='bg-blue-700 p-5'>
+        <div className='flex flex-col items-center bg-blue-700 p-3'>
             {statsData.map((item, index) => (
-                <StatsItem 
-                    key={index}
-                    value={item.value}
-                    description={item.description}
-                />
+                <div key={index}className='flex items-center mb-4 mt-1'>
+                    <BiMoneyWithdraw size={37} className='mr-2'/>
+                    <StatsItem
+                        value={item.value}
+                        description={item.description}
+                    />
+                </div>
             ))}
-        </section>
+        </div>
     );
 }
