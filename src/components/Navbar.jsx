a789c80588054266da335e2da35554b67a85836e
el634dev <84924260+el634dev@users.noreply.github.com>
2024-11-18 13:08:17 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
 import MenuIcon from './MenuIcon';
+import React from 'react';
 import { CiBellOn } from "react-icons/ci";
 import SearchInput from './SearchInput';
 
 export default function Navbar() {
     return (
-        <header className='h-16 flex justify-between md:justify-end items-center gap-4 px-4 '>
+        <header className='h-16 flex justify-between md:justify-end items-center gap-4 px-4'>
            <div className='mr-[22rem] flex justify-between mt-8'>
              {/* Company Name */}
-             <h1 className="border-gray-400 font-bold text-4xl mt-2 mr-6 text-blue-900">WAVES</h1>
+             <h1 className="border-gray-400 font-semibold text-4xl mt-2 mr-6 text-blue-900">WAVES</h1>
              <SearchInput />
            </div>
             {/* Links next to hamburger menu */}
             <div className='flex gap-4 mt-8'>
-                <button className='bg-blue-800 text-[1rem] text-white p-[0.50rem] rounded line-height hover:underline cursor-pointer flex items-center'>
+                <button className='bg-blue-800 hover:bg-blue-700 text-[1rem] text-white p-[0.50rem] rounded line-height cursor-pointer flex items-center'>
                     <CiBellOn className='mr-1'/>  News
                 </button> 
                 {/* Gift */}
-                <button className='bg-blue-800 text-[1rem] text-white p-[0.50rem] rounded line-height hover:underline cursor-pointer'>
+                <button className='bg-blue-800 hover:bg-blue-700 text-[1rem] text-white p-[0.50rem] rounded line-height cursor-pointer'>
                     Gift the Ocean
                 </button>
             </div>
             {/* Hamburger Icon */}
-            <MenuIcon />
+           <div className='mt-7'>
+             <MenuIcon />
+           </div>
         </header>
     );
 };
